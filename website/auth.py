98a5a7386d6a752f808efb64dccd6f4449965390
Samuel Adekunle <samuelbeebest@gmail.com>
2021-08-12 12:05:38 +0100

    rendering HTML

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
+@views.route('/home')
 def home():
-    return 'Home'
-    
+    return render_template('home.html')
