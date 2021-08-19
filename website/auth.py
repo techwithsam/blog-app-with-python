e1d3d6b322daeeae47dd9639de239aa94fd993d7
Samuel Adekunle <samuelbeebest@gmail.com>
2021-08-19 23:28:34 +0100

    http post methods

--- website/auth.py
+++ website/auth.py
@@ -2,11 +2,11 @@ from flask import Blueprint, render_template, redirect, url_for, flash, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     return render_template('sign-up.html')
 
