7144ffc7d0ff46eb5f4c375abcc494f37c1b3dc6
Samuel Adekunle <samuelbeebest@gmail.com>
2021-08-20 00:15:11 +0100

    creating new user

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, redirect, url_for, request
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from . import db
+from .models import User
+from flask_login import login_user, logout_user, login_required, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -10,10 +13,34 @@ def login():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    email = request.form.get("email")
-    username = request.form.get("username")
-    password1 = request.form.get("password1")
-    password2 = request.form.get("password2")
+    if request.method == 'POST':
+        email = request.form.get("email")
+        username = request.form.get("username")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+        
+        email_exist = User.query.filter_by(email=email).first()
+        username_exist = User.query.filter_by(username=username).first()
+
+        if email_exist:
+            flash('Email already exist', category='error')
+        elif username_exist:
+            flash('Username already exist', category='error')
+        elif password1 != password2:
+            flash('Password not match', category='error')
+        elif len(username) < 2:
+            flash('Username too short', category='error')
+        elif len(password1) < 6:
+            flash('Password too short', category='error')
+        elif len(email) < 4:
+            flash('Email is invalid', category='error')
+        else:
+            new_user = User(email=email, username=username, password=password1)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Sign up successfully', category='success')
+            return redirect(url_for('views.home'))
+        
     return render_template('sign-up.html')
 
 @auth.route('/logout')
