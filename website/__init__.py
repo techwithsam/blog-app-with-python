d9a6a6e0cd15e05e6e046f01ae83887f5b19ac03
Samuel Adekunle <samuelbeebest@gmail.com>
2021-08-22 23:27:59 +0100

    Database Bug Fixes

--- website/__init__.py
+++ website/__init__.py
@@ -9,7 +9,7 @@ DB_NAME = "database.db"
 def create_app():
    app = Flask(__name__)
    app.config['SECRET_KEY'] = 'samuelKey'
-   app.config['SQL_ALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+   app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
    db.init_app(app)
 
    from .views import views
