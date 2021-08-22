cef51d3d857c46e727c189a3bce239ce11385e4c
Samuel Adekunle <samuelbeebest@gmail.com>
2021-08-23 00:14:05 +0100

    Creating a Post

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
+from .models import Post
+from . import db
 
 views = Blueprint('views', __name__)
 
@@ -18,7 +20,11 @@ def create_post():
         if not text:
             flash('Post cannot be empty', category='error')
         else:
+            post = Post(text=text, author=current_user.id)
+            db.session.add(post)
+            db.session.commit()
             flash('Post created!', category='success')
+            return redirect(url_for('views.home'))
 
-            
+
     return render_template('create_post.html', user=current_user)
