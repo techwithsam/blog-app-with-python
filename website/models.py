efe8fc3334550bd6549bf34321166d45c886829c
Samuel Adekunle <samuelbeebest@gmail.com>
2021-08-22 23:52:18 +0100

    Post Database Model

--- website/models.py
+++ website/models.py
@@ -8,3 +8,9 @@ class User(db.Model, UserMixin):
    email = db.Column(db.String(150), unique=True)
    password = db.Column(db.String(128))
    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+
+class Post(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   text = db.Column(db.Text, nullable=False)
+   date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+   author = db.Column(db.Integer, db.ForeignKey('user.id'))
