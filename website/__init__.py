fb3ff71c4bed79f91ee1a4fa09ef80104daf78d5
Samuel Adekunle <samuelbeebest@gmail.com>
2021-08-23 00:03:07 +0100

    Foreign Key Relationships

--- website/__init__.py
+++ website/__init__.py
@@ -18,7 +18,7 @@ def create_app():
    app.register_blueprint(views, url_prefix='/')
    app.register_blueprint(auth, url_prefix='/')
 
-   from .models import User
+   from .models import User, Post
 
    create_database(app)
    login_manager = LoginManager()
--- website/models.py
+++ website/models.py
@@ -8,9 +8,10 @@ class User(db.Model, UserMixin):
    email = db.Column(db.String(150), unique=True)
    password = db.Column(db.String(128))
    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+   posts = db.relationship('Post', backref="user", passive_deletes=True)
 
 class Post(db.Model):
    id = db.Column(db.Integer, primary_key=True)
    text = db.Column(db.Text, nullable=False)
    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
-   author = db.Column(db.Integer, db.ForeignKey('user.id'))
+   author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
