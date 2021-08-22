6e557e93cd2725383af160bc54640f1954cae420
Samuel Adekunle <samuelbeebest@gmail.com>
2021-08-23 00:25:21 +0100

    Displaying and Deleting Posts

--- website/views.py
+++ website/views.py
@@ -9,7 +9,8 @@ views = Blueprint('views', __name__)
 @views.route('/home')
 @login_required
 def home():
-    return render_template('home.html', user=current_user)
+    posts = Post.query.all()
+    return render_template('home.html', user=current_user, posts=posts)
 
 @views.route('/create-post', methods=['GET', 'POST'])
 @login_required
