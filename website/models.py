22d886901c52a5ec937edc380a8261b1630473f8
Samuel Adekunle <samuelbeebest@gmail.com>
2021-08-24 23:40:05 +0100

    Comment Database Model

--- website/models.py
+++ website/models.py
@@ -9,9 +9,19 @@ class User(db.Model, UserMixin):
    password = db.Column(db.String(128))
    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
    posts = db.relationship('Post', backref="user", passive_deletes=True)
+   comments = db.relationship('Comment', backref="user", passive_deletes=True)
 
 class Post(db.Model):
    id = db.Column(db.Integer, primary_key=True)
    text = db.Column(db.Text, nullable=False)
    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+   comments = db.relationship('Comment', backref="post", passive_deletes=True)
+
+
+class Comment(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   text = db.Column(db.String(200), nullable=False)
+   date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+   author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+   post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete="CASCADE"), nullable=False)
