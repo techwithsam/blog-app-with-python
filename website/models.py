3f2dce1f64314e5e7f7e3d09abbefadb745b5ff8
Samuel Adekunle <samuelbeebest@gmail.com>
2021-08-27 06:14:09 +0100

    Like Databse Model

--- website/models.py
+++ website/models.py
@@ -25,3 +25,9 @@ class Comment(db.Model):
    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
    post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete="CASCADE"), nullable=False)
+
+class Like(db.Model):
+   id = db.Column(db.Integer, primary_key=true)
+   date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+   author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+   post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete="CASCADE"), nullable=False)
