25371edd094da6103b69ed6f706f500d23f455e2
Samuel Adekunle <samuelbeebest@gmail.com>
2021-08-27 14:09:15 +0100

    Like Post View

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Post, User, Comment
+from .models import Post, User, Comment, Like
 from . import db
 
 views = Blueprint('views', __name__)
@@ -97,3 +97,23 @@ def delete_comment(comment_id):
         flash('Comment deleted.', category='success')
 
     return redirect(url_for(dirHome))
+
+
+@views.route("/like-post/<post_id>", methods=['POST'])
+@login_required 
+def like(post_id):
+    post = Post.query.filter_by(id=post_id)
+    like = Like.query.filter_by(author=current_user.id, post_id=post_id).first()
+
+    if not post:
+        flash("Post does not exist.", category='error')
+    elif like:
+        db.session.delete(like)
+        db.session.commit()
+    else:
+        like = Like(author=current_user.id, post_id=post_id)
+        db.session.delete(like)
+        db.session.commit()
+
+    return redirect(url_for(dirHome))
+    
