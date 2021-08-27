e2483fb90efa47754d7cd7676d0491c136ccc7b1
Samuel Adekunle <samuelbeebest@gmail.com>
2021-08-27 14:00:47 +0100

    Like Counter

--- website/__init__.py
+++ website/__init__.py
@@ -18,7 +18,7 @@ def create_app():
    app.register_blueprint(views, url_prefix='/')
    app.register_blueprint(auth, url_prefix='/')
 
-   from .models import User, Post, Comment 
+   from .models import User, Post, Comment, Like
 
    create_database(app)
    login_manager = LoginManager()
